ba37e0f17b0d445ebafb037b356d58e5d4b5fc24
Emanuel Cerqueira <emanuelcerqueira07@gmail.com>
2017-12-26 23:31:36 -0300

    Otimizando função classificar imc

--- js/index.js
+++ js/index.js
@@ -4,28 +4,24 @@ function calcularImc(peso, altura) {
 }
 
 function classificarImc(imc) {
+    var classificao 
     if (imc <= 18.5) {
-        var classificao = "abaixo do peso";
-        return classificao;
+        classificao = "abaixo do peso";
     }
     else if (imc >= 18.6 && imc < 25) {
-        var classificao = "no peso Ideal";
-        return classificao;
+        classificao = "no peso Ideal";
     }
     else if (imc >= 25 && imc < 30) {
-        var classificao = "levemente acima do peso";
-        return classificao;
+        classificao = "levemente acima do peso";
     }
     else if (imc >= 30 && imc < 35) {
-        var classificao = "com obesidade grau I";
-        return classificao;
+        classificao = "com obesidade grau I";
     }
     else if (imc >= 35 && imc < 40) {
-        var classificao = "com obesidade grau II";
-        return classificao;
+        classificao = "com obesidade grau II";
     }
     else if (imc >= 40) {
-        var classificao = "com obesidade grau III";
-        return classificao;
+        classificao = "com obesidade grau III";
     }
+    return classificao;
 }
